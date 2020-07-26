7746645c50c2da9c166279c10c874eac76086a19
leejaeeun <leejaeeun@archive-asia.co.jp>
2020-07-26 17:31:44 +0900

    reset gitignore

--- Pods/Target Support Files/Pods-firebase_login_sample/Pods-firebase_login_sample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firebase_login_sample : NSObject
-@end
-@implementation PodsDummy_Pods_firebase_login_sample
-@end
--- Pods/Target Support Files/Pods-firebase_login_sampleTests/Pods-firebase_login_sampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firebase_login_sampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_firebase_login_sampleTests
-@end
